8f653368ac52e1ca4afdba47e54c2b25b47c6f7f
Cavichia-D-Santos <thiagogamer10000gmail.com>
2025-05-13 21:40:44 -0300

    teste

--- cypress/e2e/TesteIntegração.cy.js
+++ cypress/e2e/TesteIntegração.cy.js
@@ -1,6 +1,6 @@
 describe('Index', () => {
     beforeEach(() => {
-        cy.visit('http://127.0.0.1:5501');
+        cy.visit('/');
     });
 
     it('Deve clicar e se direcionar à página', () => {
--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -1,5 +1,5 @@
 describe('Página inicial', () => {
   it('Carregar página inicial', () => {
-    cy.visit('http://127.0.0.1:5501')
+    cy.visit('/')
   })
 })
