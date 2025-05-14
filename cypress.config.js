8500c1274968e56052a37eea5095be4c9eade320
Cavichia-D-Santos <thiagogamer10000gmail.com>
2025-05-13 21:23:38 -0300

    correcoes

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:5501/index.html',
+    baseUrl: 'https://pesquisa-emissao-co2.azurewebsites.net/',
   },
 });
