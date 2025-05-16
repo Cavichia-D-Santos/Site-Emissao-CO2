b38153130535c6def447bad623fc3b89a85a709a
Brunorwell <brunorwell12@outlook.com>
2025-05-16 18:23:20 -0300

    cy contains

--- cypress/e2e/filtrosTabela.cy.js
+++ cypress/e2e/filtrosTabela.cy.js
@@ -6,15 +6,18 @@ describe("Filtros na tabela", () => {
     it('Deve filtrar modo de locomoção por carro', () => {
         cy.get('input[type=search]').type('Carro');
         cy.get('table tbody tr');
+        cy.get('table').contains('td', 'Carro');
     });
 
         it('Deve filtrar modo de locomoção por transporte Público', () => {
         cy.get('input[type=search]').type('Transporte Público');
         cy.get('table tbody tr');
+        cy.get('table').contains('td', 'Transporte Público');
     });
 
     it('Deve filtrar modo de locomoção por moto', () => {
         cy.get('input[type=search]').type('Moto');
         cy.get('table tbody tr');
+        cy.get('table').contains('td', 'Moto');
     });
 })
